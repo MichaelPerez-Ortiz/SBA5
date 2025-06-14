4b200eedebb4df272adf42358bdc2455ad571c5c
Michael Pérez-Ortiz <perezortizm7@gmail.com>
2025-06-14 04:36:10 -0400

    Moved info to another file and imported/exported it

---
+++ data/info.js
@@ -0,0 +1,27 @@
+
+
+export const facts = [
+
+    { id: 1 , fact: "Cheetahs are the fastest land animals, capable of reaching speeds up to 75 mph." , verified: true} , 
+    { id: 2 , fact: "A cheetah's tail helps with balance and steering at high speeds." , verified: true} ,
+    { id: 3 , fact: "Cheetah cubs have a mantle of long hair that helps camouflage them in grass." , verified: true}
+
+];
+
+export const sightings= [
+
+    { id: 1 , habitat: "Serengeti National Park" , date: "2023-05-15" , count: 3} ,
+    { id: 2 , habitat: "masai mara" , date: "2023-09-1" , count: 5} ,
+    { id: 3 , habitat: "Dasht-e Kavir" , date: "2022-08-19" , count: 1} ,
+    { id: 4 , habitat: "Botswana" , date: "2018-02-02" , count: 2}
+
+];
+
+export const organizations = [
+
+    { id: 1 , name: "Cheetah Conservation Fund" , location: "Namibia" , focus: "Research and Conservation"} ,
+    { id: 2 , name: "Action for Cheetahs in Kenya" , location: "Kenya" , focus: "Community Engagement"} ,
+    { id: 3 , name: "Iranian Cheetah Society" , location: "Iran" , focus: "Asiatic Cheetah Protection"}
+
+];
+
--- index.js
+++ index.js
@@ -1,43 +1,26 @@
  import express from 'express';
 
+ import {facts , sightings , organizations} from "./data/info.js"
+
 const port = 8080
 const app = express()
 
 
 
-let facts = [
-
-    { id: 1 , fact: "Cheetahs are the fastest land animals, capable of reaching speeds up to 75 mph."} , 
-    { id: 2 , fact: "A cheetah's tail helps with balance and steering at high speeds."} ,
-    { id: 3 , fact: "Cheetah cubs have a mantle of long hair that helps camouflage them in grass."}
-
-];
-
-let sightings= [
-
-    { id: 1 , habitat: "Serengeti National Park" , date: "2023-05-15" , count: 3} ,
-    { id: 2 , habitat: "masai mara" , date: "2023-09-1" , count: 5} ,
-    { id: 3 , habitat: "Dasht-e Kavir" , date: "2022-08-19" , count: 1} ,
-    { id: 4 , habitat: "Botswana" , date: "2018-02-02" , count: 2}
-
-];
-
-let organizations = [
-
-    { id: 1 , name: "Cheetah Conservation Fund" , location: "Namibia" , focus: "Research and Conservation"} ,
-    { id: 2 , name: "Action for Cheetahs in Kenya" , location: "Kenya" , focus: "Community Engagement"} ,
-    { id: 3 , name: "Iranian Cheetah Society" , location: "Iran" , focus: "Asiatic Cheetah Protection"}
-
-];
-
-
-
-
 
 app.get("/facts" , (req , res) => {
-    console.log("Facts Reached")
-    res.json(facts)
+    let info = facts;
+    const { verified } = req.query;
+
+        if (verified === "true") {
+             info = info.filter(fact => fact.verified === true);
+    } else if (verified === "false") { 
+        info = info.filter(fact => fact.verified === false);
+    }
+
+    res.json(info)
 });
+
 
 app.get("/sightings" , (req , res) => {
     console.log("Cheetah Sightings")
